a01116d5b6fb704408486c9581bc160d3b23652c
Alex Glowacki <alglowa1@gmail.com>
2023-07-11 22:20:27 +0200

    random User-Agent header value

---
+++ domain/src/test/scala/zio/twitter/application/scraper/UtilsSpec.scala
@@ -0,0 +1,26 @@
+package zio.twitter.application.scraper
+
+import zio.http.Header.UserAgent
+import zio.test.*
+import zio.twitter.application.scraper.Utils.randUserAgent
+
+import scala.util.Random
+
+object UtilsSpec extends ZIOSpecDefault:
+
+  def spec =
+    suite("randUserAgent is correct")(
+      test("version in User-Agent header is correct") {
+        Random.setSeed(2023)
+        val versionRegex = """(\d+).0.0.0""".r
+
+        val rawUserAgent = UserAgent.render(randUserAgent)
+        versionRegex.findFirstMatchIn(rawUserAgent) match
+          case Some(version) =>
+            assertTrue(version.group(1) == "112")
+          case None          =>
+            throw new Exception(
+              "Version doesn't exist in User-Agent header"
+            )
+      }
+    )
