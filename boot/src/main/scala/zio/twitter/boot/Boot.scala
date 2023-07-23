25f46faf3672cf5117bf0798b45d275caa5c748f
Alex Glowacki <alglowa1@gmail.com>
2023-07-23 11:47:10 +0200

    for easier debugging

--- boot/src/main/scala/zio/twitter/boot/Boot.scala
+++ boot/src/main/scala/zio/twitter/boot/Boot.scala
@@ -2,6 +2,8 @@ package zio.twitter.boot
 
 import zio.*
 import zio.logging.consoleLogger
+import zio.twitter.application.scraper.TwitterSearchScraper
+import zio.twitter.application.scraper.TwitterSearchScraper.url
 import zio.twitter.commons.Logger.loggerConfig
 
 object Boot extends ZIOAppDefault:
@@ -13,6 +15,6 @@ object Boot extends ZIOAppDefault:
 
   override val run: UIO[ExitCode] =
     (for
-      _ <- ZIO.logDebug("Hello world!")
-      _ <- ZIO.logDebug("I was compiled by Scala 3. :)")
-    yield ()).exitCode
+      _ <- TwitterSearchScraper.getApiData(url) //ZIO.logDebug("Hello world!")
+      //_ <- ZIO.logDebug("I was compiled by Scala 3. :)")
+    yield ()).exitCode.provide(TwitterSearchScraper.layer, ZState.initial(TwitterSearchScraper.initstate))
